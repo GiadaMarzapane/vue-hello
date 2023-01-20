e4fb8d004fdd3b67ff93a386ab92f364faacdda9
GiadaMarzapane <giada.ortesta@gmail.com>
2023-01-20 15:39:50 +0100

    Push esercizio + bonus

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            message: 'Hello Vue!'
+            message: 'Hello Vue!',
+            image: 'https://images.unsplash.com/photo-1519664824562-b4bc73f9795a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=327&q=80'
         }
     }
 }).mount('#app');
